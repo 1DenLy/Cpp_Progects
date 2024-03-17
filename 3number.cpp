6972a9615301e91e7ddb6f5972af540d8dfb5f68
Denys_Korinovskyi <126396324+1DenLy@users.noreply.github.com>
2024-03-17 20:02:44 +0200

    Create 3number.cpp

---
+++ 3number.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace sdt;
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void evenOddSort(int arr[], int size) {
+    int left = 0, right = size - 1;
+    
+    while (left < right) {
+        while (arr[left] % 2 == 0 && left < right) {
+            left++;
+        }
+        while (arr[right] % 2 != 0 && left < right) {
+            right--;
+        }
+        if (left < right) {
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+        }
+    }
+}
+
+int main() {
+    const int size = 10;
+    int arr[size] = {2, 5, 7, 8, 10, 11, 13, 14, 16, 17};
+    
+    cout << "Original array: ";
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << std::endl;
+    
+    evenOddSort(arr, size);
+    
+    std::cout << "Array after sorting: ";
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << std::endl;
+    
+    return 0;
+}
