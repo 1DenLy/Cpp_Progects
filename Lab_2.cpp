eb307d36475b99eb253d6cafed909875c445047d
Denys_Korinovskyi <126396324+1DenLy@users.noreply.github.com>
2024-04-03 18:56:24 +0300

    Update Lab_2.cpp

--- Lab_2.cpp
+++ Lab_2.cpp
@@ -1,137 +1,129 @@
 #include <iostream>
-#include <vector>
 #include <cstdlib> 
+#include <ctime>
+
+using namespace std;
 
 void generateAndFilterNumbers() {
-    const int arraySize = 14;
-    int maxThreshold = 50;
+    const int size = 14;
+    int threshold = 50;
 
-    std::cout << maxThreshold << std::endl;
+    int nums[size];
+    int filtered[size];
+    int filteredCount = 0;
 
-    int numbers[arraySize];
-    std::vector<int> filteredNumbers;
+    cout << threshold << endl;
 
-    for (int i = 0; i < arraySize; i++) {
-        numbers[i] = -100 + rand() % 201;
-        std::cout << numbers[i] << " ";
+    for (int i = 0; i < size; i++) {
+        nums[i] = -100 + rand() % 201;
+        cout << nums[i] << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 
-    for (int i = 0; i < arraySize; i++) {
-        if (abs(numbers[i]) > maxThreshold) {
-            filteredNumbers.push_back(numbers[i]);
+    for (int i = 0; i < size; i++) {
+        if (abs(nums[i]) > threshold) {
+            filtered[filteredCount++] = nums[i];
         }
     }
 
-    for (int i = 0; i < filteredNumbers.size(); i++) {
-        std::cout << filteredNumbers[i] << " ";
+    for (int i = 0; i < filteredCount; i++) {
+        cout << filtered[i] << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 }
 
 void makeNegativePositive() {
-    const int arraySize = 14;
-    int numbers[arraySize];
+    const int size = 14;
+    int nums[size];
 
-    for (int i = 0; i < arraySize; i++) {
-        numbers[i] = -50 + rand() % 101;
-        std::cout << numbers[i] << " ";
+    for (int i = 0; i < size; i++) {
+        nums[i] = -50 + rand() % 101;
+        cout << nums[i] << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 
-    for (int i = 0; i < arraySize; i++) {
-        if (numbers[i] < 0) {
-            numbers[i] *= -1;
+    for (int i = 0; i < size; i++) {
+        if (nums[i] < 0) {
+            nums[i] *= -1;
         }
-        std::cout << numbers[i] << " ";
+        cout << nums[i] << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 }
 
 void withdrawMoney() {
-    int denominations[] = {1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
+    int denoms[] = {1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
 
     int amount;
-    std::cout << "Введіть суму для зняття: ";
-    std::cin >> amount;
+    cout << "Введіть суму для зняття: ";
+    cin >> amount;
 
     for (int i = 0; i < 10; i++) {
-        while (amount - denominations[i] >= 0) {
-            std::cout << denominations[i] << ", ";
-            amount -= denominations[i];
+        while (amount - denoms[i] >= 0) {
+            cout << denoms[i] << ", ";
+            amount -= denoms[i];
         }
     }
-    std::cout << std::endl;
+    cout << endl;
 }
 
 void mergeAndSortArrays() {
     const int size1 = 6;
     const int size2 = 8;
 
-    int array1[size1];
-    int array2[size2];
-    int mergedArray[size1 + size2];
+    int arr1[size1];
+    int arr2[size2];
+    int merged[size1 + size2];
 
     for (int i = 0; i < size1; i++) {
-        array1[i] = rand() % 100;
-        std::cout << array1[i] << " ";
+        arr1[i] = rand() % 100;
+        cout << arr1[i] << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
     for (int i = 0; i < size2; i++) {
-        array2[i] = rand() % 100;
-        std::cout << array2[i] << " ";
+        arr2[i] = rand() % 100;
+        cout << arr2[i] << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 
     int mergedIndex = 0;
     for (int i = 0; i < size1; i++) {
-        mergedArray[mergedIndex++] = array1[i];
+        merged[mergedIndex++] = arr1[i];
     }
     for (int i = 0; i < size2; i++) {
-        mergedArray[mergedIndex++] = array2[i];
+        merged[mergedIndex++] = arr2[i];
     }
 
     for (int i = 1; i < size1 + size2;) {
-        if (mergedArray[i - 1] > mergedArray[i]) {
-            int temp = mergedArray[i];
-            mergedArray[i] = mergedArray[i - 1];
-            mergedArray[i - 1] = temp;
+        if (merged[i - 1] > merged[i]) {
+            int temp = merged[i];
+            merged[i] = merged[i - 1];
+            merged[i - 1] = temp;
             i = 1;
         } else {
             i++;
         }
     }
 
-    std::cout << std::endl;
+    cout << endl;
     for (int i = 0; i < size1 + size2; i++) {
-        std::cout << mergedArray[i] << " ";
+        cout << merged[i] << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 }
 
 int main() {
-    std::cout << std::endl << "Завдання 1" << std::endl << "----------" << std::endl;
+    srand(time(nullptr));
+
+    cout << endl << "Завдання 1" << endl << "----------" << endl;
     generateAndFilterNumbers();
-    std::cout << std::endl << "Завдання 2" << std::endl << "----------" << std::endl;
+    cout << endl << "Завдання 2" << endl << "----------" << endl;
     makeNegativePositive();
-    std::cout << std::endl << "Завдання 3" << std::endl << "----------" << std::endl;
+    cout << endl << "Завдання 3" << endl << "----------" << endl;
     withdrawMoney();
-    std::cout << std::endl << "Завдання 4" << std::endl << "----------" << std::endl;
+    cout << endl << "Завдання 4" << endl << "----------" << endl;
     mergeAndSortArrays();
-    std::cout << std::endl;
+    cout << endl;
+
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
